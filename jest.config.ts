a11b4b0eee052290b8cddb7bffc9b26e5cc30fc1
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-07-08 22:07:04 +0200

    init

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { Config } from "@jest/types";
+import { join } from "path";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "~root/tsconfig.json";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  testTimeout: 100_000_000,
+  setupFilesAfterEnv: ["jest-extended"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: join(process.cwd(), "."),
+  }),
+};
+
+export default config;
