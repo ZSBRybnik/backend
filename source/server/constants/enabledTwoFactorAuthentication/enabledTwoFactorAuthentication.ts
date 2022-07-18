93a46168739d1b34cbf523af77e9a6c86660a086
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-07-18 16:05:01 +0200

    init

---
+++ source/server/constants/enabledTwoFactorAuthentication/enabledTwoFactorAuthentication.ts
@@ -0,0 +1,8 @@
+enum EnabledTwoFactorAuthentication {
+  Disabled = "disabled",
+  Application = "application",
+  Phone = "phone",
+  PhoneWithCalls = "phoneWithCalls",
+}
+
+export default EnabledTwoFactorAuthentication;
