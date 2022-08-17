4ac0904b28c453b1923d256b41c26395334c347b
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-08-17 18:12:43 +0200

    init

--- source/scripts/runner/commands/setupHusky/setupHusky.ts
+++ source/scripts/runner/commands/setupHusky/setupHusky.ts
@@ -3,5 +3,5 @@ import scriptsKeys from "~scripts/build/constants/scriptsKeys/scriptsKeys";
 import Programs from "~scripts/runner/types/programs/programs";
 
 (async (): Promise<void> => {
-  await $`${Programs.Husky} add .${Programs.Husky}/pre-commit "${Programs.Yarn} run ${scriptsKeys["lint"]} && ${Programs.Yarn} run ${scriptsKeys["test"]}" && ${Programs.Husky} add .${Programs.Husky}/post-merge "${Programs.Yarn}"`;
+  await $`${Programs.Husky} add .${Programs.Husky}/pre-commit "${Programs.Yarn} run ${scriptsKeys["lint"]} && ${Programs.Yarn} run ${scriptsKeys["test"]} && sort-package-json" && ${Programs.Husky} add .${Programs.Husky}/post-merge "${Programs.Yarn}"`;
 })();
