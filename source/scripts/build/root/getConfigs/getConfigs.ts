2d929e96c3651dfcd8bcd0b79a6d4d387eacf09a
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-08-30 16:47:20 +0200

    init

--- source/scripts/build/root/getConfigs/getConfigs.ts
+++ source/scripts/build/root/getConfigs/getConfigs.ts
@@ -1,11 +1,8 @@
 import { Configuration } from "webpack";
 import getConfig from "~backend/source/scripts/build/root/getConfig/getConfig";
-
 import ExtendedMode from "~backend/source/scripts/build/types/extendedMode/extendedMode";
-
 import Mode from "~backend/source/scripts/build/types/mode/mode";
-
-import { TargetType } from "~shared/constants/TargetType";
+import { TargetType } from "~backend/source/shared/constants/TargetType";
 
 export type ConfigsMapper = {
   [key in TargetType]: Configuration[];
