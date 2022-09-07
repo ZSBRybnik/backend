5ec354baecccd6aec4e8653d37e8a0b221c797c8
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-09-07 20:08:19 +0200

    init
    Co-authored-by: Krzysztof Zawisła <zawislakrzysztof.zk@gmail.com>

--- source/server/rest/index.ts
+++ source/server/rest/index.ts
@@ -1,55 +1,11 @@
-/* eslint-disable max-params */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import Gun /*, { GunOptions }*/ from "gun";
-//import mongoose, { Schema } from "mongoose";
 import mainPort from "~backend/source/server/rest/constants/ports/ports";
 import createServer from "~backend/source/server/rest/utils/createServer/createServer";
 import { CreateServerOuput } from "~backend/source/server/rest/utils/createServer/createServer.types";
 
-//const { Flint, NodeAdapter } = await import("gun-flint" as any);
-
 const { server, httpServer }: CreateServerOuput = createServer({
   port: mainPort,
 });
-
-//let initialized = false;
-
-/*const myGunAdapter = new NodeAdapter({
-  opt: async () => {
-    await mongoose.connect("mongodb://localhost:27017/gun");
-    mongoose.model("Gun", new Schema({}), "gun");
-    initialized = true;
-  },
-  get: async (key: string, done: (error: null, data?: unknown) => void) => {
-    if (initialized) {
-      const collection = await mongoose.connection.db.collection("gun");
-      const data = await collection.findOne({
-        _id: key,
-      });
-      console.log(data);
-      done(null, data ?? {});
-    }
-  },
-  put: async (
-    key: string,
-    node: string,
-    done: (error: null, data?: unknown) => void,
-  ) => {
-    if (initialized) {
-      const collection = await mongoose.connection.db.collection("gun");
-      await collection.updateMany(
-        { _id: key },
-        {
-          key: key,
-          val: node,
-        },
-      );
-      done(null);
-    }
-  },
-});
-
-Flint.register(myGunAdapter);*/
 
 export const gun = Gun({
   file: "gun-database",
