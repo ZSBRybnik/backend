3070156289db3520593a5248121b584f3321f89d
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-10-08 15:48:09 +0200

    init

--- source/native-addon-zig/build.zig
+++ source/native-addon-zig/build.zig
@@ -2,10 +2,10 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(builder: *Builder) void {
     const mode = builder.standardReleaseOptions();
-    const lib = builder.addStaticLibrary("native-addon-zig", "source/main.zig");
+    const lib = builder.addSharedLibrary("native-addon-zig", "source/index.zig", builder.version(0, 0, 1));
     lib.setBuildMode(mode);
     lib.install();
-    var main_tests = builder.addTest("source/main.zig");
+    var main_tests = builder.addTest("source/index.zig");
     main_tests.setBuildMode(mode);
     const test_step = builder.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
