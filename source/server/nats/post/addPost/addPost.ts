ec85f41198192d7b8561231d56111a3d75905885
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-10-11 12:33:05 +0200

    init

--- source/server/nats/post/addPost/addPost.ts
+++ source/server/nats/post/addPost/addPost.ts
@@ -1,10 +1,25 @@
 /* eslint-disable max-params */
+import { Post } from "@prisma/postgresql";
+import { Collection, Insert, Ref } from "faunadb";
+import faunaDBClient from "~backend/source/server/clients/faunaClient/faunaClient";
+import ipfsClient from "~backend/source/server/clients/ipfsClient/ipfsClient";
+import mongoDBClient from "~backend/source/server/clients/mongoDBClient/mongoDBClient";
 import natsClient, {
   jsonCodec,
 } from "~backend/source/server/clients/natsClient/natsClient";
 
 natsClient.subscribe("post.add", {
-  callback: (_error, { data }) => {
-    console.log(jsonCodec.decode(data));
+  callback: async (_error, { data }) => {
+    const post: Post = jsonCodec.decode(data) as Post;
+    const mongoDBPromise = mongoDBClient.post.create({
+      data: post,
+    });
+    const ipfsPromise = ipfsClient.add(post);
+    const [{ cid }] = await Promise.all([ipfsPromise, mongoDBPromise]);
+    await faunaDBClient.query(
+      Insert(Ref(Collection("posts")), 1, "create", {
+        data: { cid, id: post.id },
+      }),
+    );
   },
 });
--- source/server/rest/routes/post/addPostHandler/addPostHandlerErrorCodes.ts
+++ source/server/rest/routes/post/addPostHandler/addPostHandlerErrorCodes.ts
@@ -1,6 +1,4 @@
 import { NextFunction } from "express";
-import ipfsClient from "~backend/source/server/clients/ipfsClient/ipfsClient";
-import mongoDBClient from "~backend/source/server/clients/mongoDBClient/mongoDBClient";
 import natsClient, {
   jsonCodec,
 } from "~backend/source/server/clients/natsClient/natsClient";
@@ -24,19 +22,7 @@ const addPostHandlerErrorCodes = async ({
       data: { ...rest, content, brief: brief || content.slice(0, 150) },
     });
     natsClient.publish("post.add", jsonCodec.encode(post));
-    const mongoDBPromise = mongoDBClient.post.create({
-      data: post,
-    });
-    const ipfsPromise = ipfsClient.add(post);
-    const [{ cid }] = await Promise.all([ipfsPromise, mongoDBPromise]);
-    console.log(cid);
-    //await faunaDBClient.query(
-    //Insert(Ref(Collection("posts")), 1, "create", {
-    //   data: { cid, id: post.id },
-    // }),
-    // );
-  } catch (e) {
-    console.error(e);
+  } catch {
     response.sendStatus(400);
     return next();
   }
