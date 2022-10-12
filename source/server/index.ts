a6d953d5b4fc5985f67675d617149d29e74dc1e9
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-10-12 18:03:26 +0200

    init

--- source/server/index.ts
+++ source/server/index.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import "source-map-support/register";
 import "v8-compile-cache";
-import "~backend/source/server/clients/natsClient/natsClient";
 import "~backend/source/server/nats";
 import "~backend/source/server/rest";
--- source/server/nats/index.ts
+++ source/server/nats/index.ts
@@ -1,2 +1,3 @@
+import "~backend/source/server/clients/natsClient/natsClient";
 import "~backend/source/server/nats/page/addPage/addPage";
 import "~backend/source/server/nats/post/addPost/addPost";
