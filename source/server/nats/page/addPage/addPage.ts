ab5158f0d6b0f48448435ff3c4b21e5a8d389f22
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-10-16 22:04:40 +0200

    init

--- source/server/nats/page/addPage/addPage.ts
+++ source/server/nats/page/addPage/addPage.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-params */
 import { Page } from "@prisma/postgresql";
-import { Collection, Insert, Ref } from "faunadb";
+import { Collection, Create } from "faunadb";
 import faunaDBClient from "~backend/source/server/clients/faunaClient/faunaClient";
 import ipfsClient from "~backend/source/server/clients/ipfsClient/ipfsClient";
 import mongoDBClient from "~backend/source/server/clients/mongoDBClient/mongoDBClient";
@@ -10,19 +10,22 @@ import natsClient, {
 
 natsClient.subscribe("page.add.*", {
   callback: async (_error, { data }) => {
-    const { id, ...pageData }: Page = jsonCodec.decode(data) as Page;
+    const { id, name, ...pageData }: Page = jsonCodec.decode(data) as Page;
     try {
       const mongoDBPromise = mongoDBClient.page.create({
-        data: { id, ...pageData },
+        data: { id, name, ...pageData },
       });
-      const ipfsPromise = ipfsClient.add(JSON.stringify({ id, ...pageData }));
+      const ipfsPromise = ipfsClient.add(
+        JSON.stringify({ id, name, ...pageData }),
+      );
       const [{ cid }] = await Promise.all([ipfsPromise, mongoDBPromise]);
       await faunaDBClient.query(
-        Insert(Ref(Collection("pages")), 1, "create", {
-          data: { cid, id },
+        Create(Collection("pages"), {
+          data: { cid: cid.toString(), name },
         }),
       );
-    } catch {
+    } catch (error) {
+      console.error(error);
       natsClient.publish(`page.add.${id}`, data);
     }
   },
