57af38ec7f12be5cef413568053a208314114bb5
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-10-16 22:38:53 +0200

    init

--- source/server/nats/post/updatePost/updatePost.ts
+++ source/server/nats/post/updatePost/updatePost.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-params */
 import { Post } from "@prisma/postgresql";
-import { Collection, Create } from "faunadb";
+import { Index, Match, Update } from "faunadb";
 import faunaDBClient from "~backend/source/server/clients/faunaClient/faunaClient";
 import ipfsClient from "~backend/source/server/clients/ipfsClient/ipfsClient";
 import mongoDBClient from "~backend/source/server/clients/mongoDBClient/mongoDBClient";
@@ -8,22 +8,23 @@ import natsClient, {
   jsonCodec,
 } from "~backend/source/server/clients/natsClient/natsClient";
 
-natsClient.subscribe("post.add.*", {
+natsClient.subscribe("post.update.*", {
   callback: async (_error, { data }) => {
-    const { id, ...postData }: Post = jsonCodec.decode(data) as Post;
+    const { id, ...pageData }: Post = jsonCodec.decode(data) as Post;
     try {
-      const mongoDBPromise = mongoDBClient.post.create({
-        data: { id, ...postData },
+      const mongoDBPromise = mongoDBClient.page.update({
+        data: { id, ...pageData },
+        where: { id },
       });
-      const ipfsPromise = ipfsClient.add(JSON.stringify({ id, ...postData }));
+      const ipfsPromise = ipfsClient.add(JSON.stringify({ id, ...pageData }));
       const [{ cid }] = await Promise.all([ipfsPromise, mongoDBPromise]);
       await faunaDBClient.query(
-        Create(Collection("posts"), {
+        Update(Match(Index("posts_by_id"), id), {
           data: { cid: cid.toString(), id },
         }),
       );
-    } catch (error) {
-      natsClient.publish(`post.add.${id}`, data);
+    } catch {
+      natsClient.publish(`post.update.${id}`, data);
     }
   },
 });
