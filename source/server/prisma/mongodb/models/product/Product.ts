e755b8d4540570f9081bc1246c1278fc8d7896ed
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-11-26 19:28:59 +0100

    init

---
+++ source/server/prisma/mongodb/models/product/Product.ts
@@ -0,0 +1,23 @@
+import { createModel } from "schemix";
+import generatePrismaString from "../../../utils/generatePrismaString/generatePrismaString";
+
+const model = createModel((ProductModel) => {
+  ProductModel.string("mongo_id", {
+    map: "_id",
+    raw: generatePrismaString({
+      rawString: `#prisma 
+       @id @default(auto()) @database.ObjectId
+      `,
+    }),
+  })
+    .int("id", {
+      unique: true,
+    })
+    .string("name")
+    .float("price")
+    .int("quantity")
+    .string("description")
+    .map("products");
+});
+
+export default model;
