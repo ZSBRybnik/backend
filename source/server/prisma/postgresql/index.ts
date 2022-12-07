55c253fc64a7ef017f595efdc3e227ed9d7fec19
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-12-07 21:48:19 +0100

    init Co-authored-by: Krzysztof Zawisła <zawislakrzysztof.zk@gmail.com>

--- source/server/prisma/postgresql/index.ts
+++ source/server/prisma/postgresql/index.ts
@@ -6,17 +6,29 @@ createSchema({
   datasource: {
     provider: "postgresql",
     url: { env: "POSTGRESQL_URL" },
+    /*extensions: generatePrismaString({
+      rawString: `#prisma
+        [pg_tgrm, zombodb]
+      `,
+    }),*/
   },
   generator: [
     {
       name: "client",
       provider: "prisma-client-js",
       output: "../../../node_modules/@prisma/postgresql",
+      // previewFeatures: ["postgresqlExtensions"],
     },
     {
       name: "tables",
       provider: "node node_modules/prisma-enum-generator",
       output: "../../../node_modules/@prisma/postgresql-tables",
+    },
+    {
+      name: "dbml",
+      provider: "prisma-dbml-generator",
+      outputName: "postgresql.dbml",
+      output: "../../../uml",
     },
   ],
 }).export(join(__dirname, ".."), "postgresql");
