b5ef829db5b241d30f9ac08a7fb5e69c85a85b12
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-12-09 21:16:13 +0100

    init

--- source/server/prisma/mongodb/models/user/User.ts
+++ source/server/prisma/mongodb/models/user/User.ts
@@ -6,6 +6,7 @@ import authenticationType, {
 import rolesEnum from "../../enums/roles/Roles";
 import userModifiers from "../../enums/userModifiers/UserModifiers";
 import postModel from "../post/Post";
+import classModel from "../schoolClass/SchoolClass";
 
 const model: PrismaModel = createModel((UserModal: PrismaModel): void => {
   UserModal.string("mongo_id", {
@@ -21,24 +22,21 @@ const model: PrismaModel = createModel((UserModal: PrismaModel): void => {
       unique: true,
     })
     .boolean("isDisabled", { map: "is_disabled" })
-
     .enum("modifiers", userModifiers, { list: true })
     .enum("roles", rolesEnum, { list: true })
     .enum("enabledTwoFactorAuthentication", authenticationType, {
       default: AuthenticationTypes.Application,
       map: "enabled_two_factor_authentication",
     })
-    /*
-    .relation("class", ClassModel, {
+    .relation("class", classModel, {
       fields: ["classId"],
       references: ["id"],
     })
     .int("classId", {
       map: "class_id",
-          .relation("orders", orderModel, { list: true })
-    })*/
+    })
+    //          .relation("orders", orderModel, { list: true })
     .relation("posts", postModel, { list: true })
-
     .string("login", {
       unique: true,
     })
