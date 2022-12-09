350dbbfa866bef5bc4a4a78301ebc1d92b3e5401
borys-malinowski <bmalinowski.cisco2017@gmail.com>
2022-12-09 22:00:08 +0100

    init

--- source/scripts/runner/commands/migrate/migrate.ts
+++ source/scripts/runner/commands/migrate/migrate.ts
@@ -7,5 +7,12 @@ import { $ } from "zx";
     $.shell = "cmd";
     $.prefix = "";
   }
-  await $`prisma db push --schema=./source/server/prisma/mongodb.prisma --force-reset && prisma db push --schema=./source/server/prisma/postgresql.prisma --accept-data-loss`;
+  const prismaPostgreSQLMigratePromise = $`prisma db push --schema=./source/server/prisma/mongodb.prisma --force-reset`;
+  const prismaMongoDBMigratePromise = $`prisma db push --schema=./source/server/prisma/postgresql.prisma --accept-data-loss`;
+  const faunaDBMigratePromise = $`cross-env TS_NODE_PROJECT=tsconfig.json ts-node ./source/server/faunadb/index.ts`;
+  await Promise.all([
+    prismaPostgreSQLMigratePromise,
+    prismaMongoDBMigratePromise,
+    faunaDBMigratePromise,
+  ]);
 })();
