f5014099c225e8ebceb83ad3ab5b1374b743f53a
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2023-03-19 12:43:31 +0100

    init

--- source/server/emails/components/email/email.tsx
+++ source/server/emails/components/email/email.tsx
@@ -1,7 +1,5 @@
-import { useHookstate } from "@hookstate/core";
 import { FunctionComponent, PropsWithChildren } from "react";
 import { I18nextProvider } from "react-i18next";
-import translationStore from "~backend/source/server/stores/translationStore/translationStore";
 import GlobalStyles from "../globalStyles/globalStyles";
 import Header from "../header/header";
 
@@ -10,9 +8,8 @@ type EmailProperties = PropsWithChildren;
 const Email: FunctionComponent<EmailProperties> = ({
   children,
 }: EmailProperties): JSX.Element => {
-  const translation = useHookstate(translationStore);
   return (
-    <I18nextProvider i18n={translation.get()}>
+    <I18nextProvider i18n={{} as any}>
       <GlobalStyles />
       <Header />
       {children}
