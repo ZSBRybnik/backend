cfd61249c902aa99614cf40ba5a41704731c6f22
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2023-04-06 13:45:42 +0200

    init

--- source/native-addon-python/source/main.py
+++ source/native-addon-python/source/main.py
@@ -1,11 +1,12 @@
 import torch
 from lavis.models import load_model_and_preprocess
 from PIL import Image
-raw_image = Image.open("example.png").convert("RGB")
+raw_image = Image.open("./example.png").convert("RGB")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
+print(device)
+model, vis_processors, _ = load_model_and_preprocess(name="blip2_t5", model_type="caption_coco_flant5xl", is_eval=True, device=device)
 image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-model.generate({"image": image})
+print(model.generate({"image": image}))
 
 def example():
   print("hello world")
